0f55afa838aa7317da80480b0997846e6da7c774
jordanlevy001 <jordan.levy001@gmail.com>
2021-09-29 10:12:18 -0700

    adding retirement_info.csv

--- queries.sql
+++ queries.sql
@@ -1,3 +1,19 @@
 -- Queries.sql
 
 SELECT * FROM departments;
+
+SELECT * FROM employees;
+
+SELECT * FROM managers;
+
+SELECT * FROM salaries;
+
+SELECT * FROM dept_emp;
+
+SELECT * FROM titles;
+
+-- Determining Retirement Eligibility
+
+SELECT * FROM retirement_info;
+
+SELECT * FROM departments;
